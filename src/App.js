03e405955336b70c946c72d89bedd7a56fae2ea1
Juliana França <julianafsilvaju@gmail.com>
2023-07-18 17:23:57 -0300

    Configurações da rota

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { Navigation } from "./components/navigation";
+import HomePage from "./pages/home";
+import LoginPage from "./pages/login";
 
 const App = () => {
   return (
     <>
       <Router>
         <Navigation />
-
+        
         <Routes>
-          <Route path="/" element={<p>Hello World!</p>} />
+          <Route path="/" element={<HomePage />} />
+          <Route path="/loginpage" element={<LoginPage />} />
         </Routes>
       </Router>
     </>
